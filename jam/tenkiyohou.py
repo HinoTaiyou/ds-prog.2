6ae8854aa9c854fe2a24994531673a85e81d4bcb
s2422083@stu.musashino-u.ac.jp <s2422083@stu.musashino-u.ac.jp>
2024-12-17 12:16:58 +0900

    DBファイルのパスを指定

--- jam/tenkiyohou.py
+++ jam/tenkiyohou.py
@@ -6,3 +6,9 @@ from typing import Dict
 
 # 地域コードをキーにして地域名を取得できるようにする
 area_cache: Dict[str, Dict] = {}
+
+class WeatherDB:
+    def __init__(self, db_path="weather.db"):
+        # DBファイルのパスを指定
+        self.db_path = db_path
+        self.init_db()
